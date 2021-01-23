36b34e40427048c716a1b416bc8ae1d6ac1ca64c
Rubem <rubemalecks@gmail.com>
2021-01-22 22:56:42 -0300

    ex32.py

---
+++ GEEK UNIVERSITY/Seção 05 - Estruturas Lógicas e Condicionais em Python/ex32.py
@@ -0,0 +1,7 @@
+"""Escreva um programa que leia o código do produto escolhido do cardápio de uma lanchonete e a
+ quantidade. O programa deve calcular o valor a ser pago por aquele lanche. Considere que cada
+  execução somente será calculado um pedido. [O cardápio da Lanchonete esta no pdf]"""
+
+cardapio = {('101','Cachorro Quente'): 1.20}
+
+print(cardapio[('101', 'Cachorro Quente')])
