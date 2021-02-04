a7b950125710839008dbadf8b9dfedc72fc499af
Rubem <rubemalecks@gmail.com>
2021-02-03 21:29:02 -0300

    Manipulation Strings

--- CURSO EM VIDEO/Aula 09 - Manipulating Text/Transformação.py
+++ CURSO EM VIDEO/Aula 09 - Manipulating Text/Transformação.py
@@ -1,12 +1,14 @@
 frase = str('Curso em Video Python')
-print(frase.replace('Python', 'Android')) #substitui frase por android
-print(frase.upper()) #upper é um metodo() - transforma tudo em maiusculo
-print(frase.lower()) #lower()tudo para minusculo
-print(frase.capitalize()) #capitalize() td p minusculo e primeira letra p maiusculo
-print(frase.title()) #title() - Todas as letras após o espaço maiusculo
+print(frase.replace('Python', 'Android'))  # substitui python por android
+print(frase.upper())  # upper é um metodo() - transforma tudo em maiusculo
+print(frase.lower())  # lower()tudo para minusculo
+# capitalize() td p minusculo e primeira letra p maiusculo
+print(frase.capitalize())
+print(frase.title())  # title() - Todas as letras após o espaço maiusculo
 frase = '   Aprenda Python  '
-print(frase.strip()) #strip() - cria uma lista com separador e excliu os espaços no inicio e final
-dividido = frase.sprit()
+# strip() - cria uma lista com separador e excliu os espaços no inicio e final
+print(frase.strip())
+dividido = frase.strip()
 print(dividido[2][3])
-print(frase.rstrip()) #rstrip() - excliu os espaços da direita
-print(frase.lstrip()) #lstrip() - excliu os espaços da esquerda
+print(frase.rstrip())  # rstrip() - excliu os espaços da direita
+print(frase.lstrip())  # lstrip() - excliu os espaços da esquerda
