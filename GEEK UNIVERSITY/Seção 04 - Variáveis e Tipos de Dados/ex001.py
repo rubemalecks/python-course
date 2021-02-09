b91a5fa84f67a565321fb6549f6c864a04c381a2
Rubem <rubemalecks@gmail.com>
2021-02-08 22:43:09 -0300

    ex001.py

--- GEEK UNIVERSITY/Seção 04 - Variáveis e Tipos de Dados/ex001.py
+++ GEEK UNIVERSITY/Seção 04 - Variáveis e Tipos de Dados/ex001.py
@@ -1,4 +1,4 @@
-'''Faça um programa que leia um numero inteiro e o imprima.'''
+'''Faça um programa que leia um numero inteiro e o imprima.\n'''
 
 num = int(input('Digite um numero inteiro: '))
 print(num)
