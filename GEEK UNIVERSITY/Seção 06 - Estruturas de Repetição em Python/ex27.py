14548128fd6afcfcdba1ea64d09d4d7f502b54bb
Rubem <rubemalecks@gmail.com>
2021-02-12 00:02:52 -0300

    .\ex27.py

--- GEEK UNIVERSITY/Seção 06 - Estruturas de Repetição em Python/ex27.py
+++ GEEK UNIVERSITY/Seção 06 - Estruturas de Repetição em Python/ex27.py
@@ -5,6 +5,7 @@ h(n)= 1 + 1/2 + 1/3 + 1/4 + ... 1/n""")
 
 n = int(input('Digite um numero: '))
 n_hrm = 0
-for c in range(1, n):
+for c in range(1, n+1):
     n_hrm += (1/c)
+    d= c
 print(f'Resposta: {n_hrm}')
