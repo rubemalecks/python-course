3a3ae869ebc1ff575ba948f6e859be41111254b9
Rubem <rubemalecks@gmail.com>
2021-02-13 23:34:48 -0300

    CURSO EM VIDEO

--- CURSO EM VIDEO/Aula 16 - Tuplas/testes/test72.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-
-lanche = ('hamburguer','suco', 'pizza', 'pudim')
-for comida in lanche: 
-    sleep(5)
-    print(f'Eu vou comer {comida}')
-sleep(2)
-print('Comi demais!!!!!!!!')
