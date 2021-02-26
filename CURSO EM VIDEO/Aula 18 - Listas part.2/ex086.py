2f3b327a9a3587f3cdb55bc97bce85c2200bbedd
Rubem <rubemalecks@gmail.com>
2021-02-25 23:40:22 -0300

    ex086.py

--- CURSO EM VIDEO/Aula 18 - Listas part.2/ex086.py
+++ CURSO EM VIDEO/Aula 18 - Listas part.2/ex086.py
@@ -6,7 +6,16 @@ lidos pelo teclado:
 |___|___|___|
 |___|___|___|
 
-Ao final, mostre a matriz na tela, com a formatação correta em ordem crescente.
-Ler os dados/ guardar em uma estrutura unica/ mostrar a estrutura
+Ao final, mostre a matriz na tela, com a formatação correta.\n
 ''')
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
 
+for linha in range(0,3):
+    for coluna in range(0,3):
+        matriz[linha][coluna] = int(input(f'Digite p/ {linha,coluna}: '))
+print('='*42)
+print()
+for linha in range(0,3):
+    for coluna in range(0,3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
+    print()    
