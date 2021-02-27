8461121d02b84010874b2d332d89535e8ae086d6
Rubem <rubemalecks@gmail.com>
2021-02-27 00:38:07 -0300

    ex088.py

--- CURSO EM VIDEO/Aula 18 - Listas part.2/ex088.py
+++ CURSO EM VIDEO/Aula 18 - Listas part.2/ex088.py
@@ -4,3 +4,32 @@ perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 par
 jogo, cadastrando tudo em uma lista composta.
 
 ''')
+numero_sorteado = list()
+jogo_sorteado = list()
+from time import sleep
+from random import randint
+q_jogos = int(input('Quantidade de Jogos: '))
+#print('Sorteando...', end=''), sleep(2)
+print()
+for c in range(0, q_jogos):
+    for i in range(0,6):
+        numero_sorteado.append(randint(1,60))
+        if numero_sorteado not in jogo_sorteado:
+            jogo_sorteado.append(numero_sorteado[:])
+            numero_sorteado.clear()
+        else:
+            while True:
+                if numero_sorteado in jogo_sorteado:
+                    numero_sorteado.clear()
+                    numero_sorteado.append(randint(1,60)) 
+                else:
+                    break                 
+            jogo_sorteado.append(numero_sorteado[:])
+            numero_sorteado.clear()
+
+    print(f'{jogo_sorteado}')
+    jogo_sorteado.clear()
+    print('...')
+    #sleep(3)
+
+    
