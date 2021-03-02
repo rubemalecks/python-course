ae859a4e1077d54f739266dc3def28c02f6223fa
Rubem <rubemalecks@gmail.com>
2021-03-02 00:19:15 -0300

    .\ex087.py

--- CURSO EM VIDEO/Aula 18 - Listas part.2/ex087.py
+++ CURSO EM VIDEO/Aula 18 - Listas part.2/ex087.py
@@ -4,13 +4,10 @@ Aprimore o desafio anterior, mostrando no final:
 (A) A soma de todos os valores pares digitados.
 (B) A soma dos valores da terceira coluna.
 (C) O maior valor da segunda linha.
-
 ''')
 
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
-maior2linha = 0
-soma_pares = 0
-soma3coluna = 0
+maior2linha = soma_pares = soma3coluna = 0
 
 for linha in range(0, 3):
     for coluna in range(0,3):
