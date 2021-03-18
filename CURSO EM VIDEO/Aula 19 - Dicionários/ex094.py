9d3d35480c105d178b5b4fc031317bc6048cd777
Rubem <rubemalecks@gmail.com>
2021-03-17 22:53:41 -0300

    Ex90,91 Dicionários

---
+++ CURSO EM VIDEO/Aula 19 - Dicionários/ex094.py
@@ -0,0 +1,9 @@
+"""
+Unindo Dicionários e Listas
+Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de
+cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
+A) Quantas pessoas cadastradas.
+B) A média de idade.
+C) Uma lista com mulheres.
+D) Uma lista com idade acima da média.
+"""
---
+++ CURSO EM VIDEO/Aula 19 - Dicionários/ex095.py
@@ -0,0 +1,5 @@
+"""
+Aprimorando os Dicionários
+Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo
+um sistema de visualização de detalhes do aproveitamento de cada jogador.
+"""
