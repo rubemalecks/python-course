8553be0e2be605623c986faf2b4802b36b3cfaa4
Rubem <rubemalecks@gmail.com>
2021-03-19 23:37:44 -0300

    ex80.py

--- CURSO EM VIDEO/Aula 17 - Listas/ex80.py
+++ CURSO EM VIDEO/Aula 17 - Listas/ex80.py
@@ -1,6 +1,6 @@
 print("""
 Exercício Python 080: 
-Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista,
+Crie um programa onde o usuário possa digitar cinco nes numéricos e cadastre-os em uma lista,
 já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 """
 )
