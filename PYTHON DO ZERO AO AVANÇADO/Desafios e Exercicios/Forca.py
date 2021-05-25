ec0bca58ef2a65eb0d3a7da4cdafa5e207032e40
rubem <rubemalecks@gmail.com>
2021-05-24 23:50:43 -0300

    Forca.py

--- PYTHON DO ZERO AO AVANÇADO/Desafios e Exercicios/Forca.py
+++ PYTHON DO ZERO AO AVANÇADO/Desafios e Exercicios/Forca.py
@@ -1,17 +1,17 @@
-palavra_secreta = 'rrerrr'.upper()
+palavra_secreta = 'rubem'.upper()
 digitados = []
 chances = 3
 while True:
-    print('-'*23)
-    if chances < 0:
+    
+    if chances <= 0:
         print('VOCÊ PERDEU!!')
         break
     secreto_temp = ''
 
 
     dig_letra = input('Digite UMA LETRA: ').upper()
+    print('-'*23) 
     if not dig_letra.isalpha() or len(dig_letra) > 1 or len(dig_letra) == 0:
-        print('-'*23)
         print('POR FAVOR, DIGITE UMA LETRA!!')
         print('-'*23)
         continue
@@ -19,33 +19,36 @@ while True:
         print('ESTA LETRA JÁ FOI ENCONTRADA, TENTE OUTRA!')
         continue
 
+        
+    digitados.append(dig_letra)
+    if dig_letra in palavra_secreta:    # se a letra estiver na palavra secreta
+        print(f'TEMOS {dig_letra} NA PALAVRA SECRETA!')
+        print('-'*23)
+    else:
+        chances -= 1    
 
-    else:        
-        digitados.append(dig_letra)
-        if dig_letra in palavra_secreta:    # se a letra estiver na palavra secreta
-            for letra_secreta in palavra_secreta:   # para cada letra na palavra
-                if letra_secreta in digitados:          # fazer o teste se é == a letra digitada, se for ...
-                    secreto_temp += letra_secreta       # secreto_temporario recebe a letra quantas vezes for necessario
+        print('NÃO TEMOS ESSA LETRA NA PALAVRA SECRETA!')                                            
+        digitados.pop() 
+        print('-'*23)
+        if chances >= 1:
+            print(f'VOCÊ AINDA TEM {chances} CHANCES!!')
+            print('-'*23)
+
+
+                                                 
+    for letra_secreta in palavra_secreta:  
+        if letra_secreta in digitados:          # fazer o teste se é == a letra digitada, se for ...
+            secreto_temp += letra_secreta       # secreto_temporario recebe a letra quantas vezes for necessario
                     
-                else:
-                    secreto_temp += '*'                 # caso contrario, concatenar '*'
-            if dig_letra in palavra_secreta:
-                print('-'*23)    
-                print(f'TEMOS {dig_letra} NA PALAVRA SECRETA!')
-        else:
-            chances -= 1                                                # cada letra errada -1 chances
-            digitados.pop()                                             # e remover da variavel digitados
-
+        else:    
+            secreto_temp += '*'                 # caso contrario, concatenar '*'
+    
+    
+    print(f'Palavra Secreta: {secreto_temp}')
     print('-'*23)
-    print(secreto_temp)
-
-
-    print('-'*23)
-    print(f'VOCÊ TEM {chances} CHANCES!!')
-    print('-'*23)
-
-
     if secreto_temp == palavra_secreta:
+        print('VOCÊ GANHOU !!!')
         break
-print('-'*23)
-print('ACERTOU!')
+       
+                
+        
