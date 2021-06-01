a9deacfe8c3103eb460bd04b7089b106e33c72e5
rubem <rubemalecks@gmail.com>
2021-05-31 23:52:30 -0300

    ex098.py

--- CURSO EM VIDEO/Aula 20 - Funções (part.1)/ex098.py
+++ CURSO EM VIDEO/Aula 20 - Funções (part.1)/ex098.py
@@ -8,3 +8,40 @@ b) de 10 até 0, de 2 em 2
 c) Uma contagem personalizada
 --> print(f"{valor} ", end="")  # Exemplo de print espaçado
 """
+from time import sleep
+
+def linha():
+    print('=-'*23)
+def enunciado(i, f, p):
+    linha()
+    if p < 0:
+        print(f'Contagem de {i} até {f}, de {p*-1} em {p*-1}:')
+    else:
+        print(f'Contagem de {i} até {f}, de {p} em {p}:')
+def contador(i, f, p):
+    if p == 0:
+        p = 1
+    enunciado(i, f, p)
+    if f < i: 
+        if p > 0:
+            p *= -1
+        for x in range(i, f-1, p):
+            print(x,end=' ')
+        print('... FIM!!!')
+    else:
+        if p < 0:
+            p *= -1
+        for x in range(i, f+1, p):
+            print(x,end=' ')
+        print('... FIM!!!')
+    linha()
+
+contador(1,10,1)
+contador(10,0,2)
+
+print('Agora é sua vez de personalizar a contagem!')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))
+salto = int(input('Salto: '))
+
+contador(inicio, fim, salto)
--- CURSO EM VIDEO/Aula 20 - Funções (part.1)/ex099.py
+++ CURSO EM VIDEO/Aula 20 - Funções (part.1)/ex099.py
@@ -3,3 +3,4 @@ Faça um programa que tenha uma função chamada maior(), que receba vários par
  com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual 
  deles é o maior.
 """
+
