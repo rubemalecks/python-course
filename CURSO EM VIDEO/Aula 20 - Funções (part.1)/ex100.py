b0498ca18cd82175cbff5b523a18852bd3c8c576
rubem <rubemalecks@gmail.com>
2021-06-03 23:28:00 -0300

    ex100.py

--- CURSO EM VIDEO/Aula 20 - Funções (part.1)/ex100.py
+++ CURSO EM VIDEO/Aula 20 - Funções (part.1)/ex100.py
@@ -6,21 +6,26 @@ pares sorteados pela função anterior.
 """
 
 from random import randint
-
+from time import sleep
 def sorteia(lista):
+    print('Sorteando 5 números: ', end='')
     for n in range(5):
-        lista.append(randint(0,9))
+        aleatorio = (randint(0,9))
+        print(f'{aleatorio}', end=' ')
+        sleep(0.9)
+        lista.append(aleatorio)
+    print()
 
 
 numeros_sorteados = list()
 sorteia(numeros_sorteados)
-print(numeros_sorteados)
+
 
 def soma_par(lista_numeros):
     soma = 0
     for num in lista_numeros:
         if num % 2 == 0:
             soma+= num
-    return soma
+    print(f'Somando os valores pares entre: {lista_numeros}, é {soma}')
 
-print(soma_par(numeros_sorteados))
+soma_par(numeros_sorteados)
